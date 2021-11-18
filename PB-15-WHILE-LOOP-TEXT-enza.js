db9ab007329db7ea865a4e2c87d9f1902a81ef8a
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-18 13:35:48 +0100

    langsam kapiere ich..

--- PB-15-WHILE-LOOP-TEXT-enza.js
+++ PB-15-WHILE-LOOP-TEXT-enza.js
@@ -4,7 +4,7 @@ let myRandomText = "ABC";
 //-2-
 while (myRandomText.length <= 50000){
  //   console.log("a" + "b" + "c");
- myRandomText= myRandomText+"X" + "x" + "o" + "8" + "o" + "8" +  "x" + "X" + ".-.";
+ myRandomText= myRandomText+"X" + "x" + "o" + "8" + "|" + "8" + "o" + "x" + "X" + ".-.";
 }
 console.log(myRandomText);
 
