3723226bff867abbca94ee010040232de26b8157
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-18 13:28:13 +0100

    Orhan's lösung

---
+++ whilelooptext.js
@@ -0,0 +1,14 @@
+let myRandomText = "abc";
+
+while (myRandomText.length < 500) {
+    myRandomText = myRandomText + "abc";
+}
+
+console.log(myRandomText);
+
+
+while (myRandomText.length < 50000) {
+    myRandomText = myRandomText + "abc";
+}
+
+console.log(myRandomText);
