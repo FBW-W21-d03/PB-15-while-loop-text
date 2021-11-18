138546a71e71ca92ea9ade55709c64e4e066f512
slavablack <veaceslav.ciornii@magenta.de>
2021-11-18 13:30:32 +0100

    abc

---
+++ pb15.js
@@ -0,0 +1,13 @@
+// Teil 1
+let myRandomText="abc";
+while (myRandomText.length<500) {
+    myRandomText=myRandomText + "a"+"b"+"c"+"d"
+};
+console.log("a"+"b"+"c");
+
+ // Teil 2
+ let myRandomText2="abc";
+while (myRandomText2.length<50000) {
+    myRandomText2=myRandomText2 + "a"+"b"+"c"+"d"+"e"
+ };
+ console.log(myRandomText2);
