30eab96ff0d62d36fab6128a7aadb6af3f948dcd
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-18 13:43:49 +0100

    Mago

---
+++ index.js
@@ -0,0 +1,28 @@
+/*Teil 1
+let zahl= 0;
+while (zahl<100) {
+    console.log(zahl);
+    zahl= zahl +1;
+    
+}
+
+//Teil 2
+let zahlzwei= 0;
+while (zahlzwei<1000) {
+    console.log(zahlzwei);
+    zahlzwei=zahlzwei +1;
+    
+}
+
+/* -- let isLogged = true;
+while (isLogged = true){
+console.log(isLogged)};  */
+
+
+let myRandomText = "abc";
+
+while ( myRandomText.length < 5000) { myRandomText = myRandomText + "a" + "b" + "c";
+console.log(myRandomText);
+
+}
+
