b71fc19983d6e96ace1e91bee3af22cd83bfd29e
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-18 13:22:58 +0100

    joa..

---
+++ PB-15-WHILE-LOOP-TEXT-enza.js
@@ -0,0 +1,13 @@
+//1.) -1-
+let myRandomText = "ABC";
+
+//-2-
+while (myRandomText.length <= 50000){
+ //   console.log("a" + "b" + "c");
+ myRandomText= myRandomText+"a" + "b" + "c" + "d";
+}
+console.log(myRandomText);
+
+//2.)
+// [Done] exited with code=0 in 0.089 seconds
+//2. mal -> [Done] exited with code=0 in 0.114 seconds
