e45a8ea216f7991da9dff99e99e44946e0ba8e62
sprotte <metatron1972@live.de>
2021-11-18 13:46:57 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,8 @@
+let myRandomText = "hallo";  
+while(myRandomText.length <= 499){
+myRandomText = myRandomText + "a" + "b" + "c";  
+console.log(myRandomText.length);
+}   
+console.log(myRandomText);
+
+
