62e23bbdad75e6ebc15926f9718b72d706754139
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-18 13:35:39 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,17 @@
+let myRandomText = "abc";
+
+while (myRandomText.length <= 500) {
+
+    myRandomText = myRandomText + "a"+ "b" +"c";
+   
+}
+console.log(myRandomText);
+
+
+
+while (myRandomText.length <= 50000) {
+
+    myRandomText1 = myRandomText1 +"a" +"b" +"c";
+    
+}
+console.log(myRandomText);
