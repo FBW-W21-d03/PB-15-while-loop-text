44288ebc55d1221b86acf55c3752d5c5347b75f4
H94ssein44 <alali.52@yahoo.com>
2021-11-18 13:56:45 +0100

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+let myRandomText =  "abc";
+
+while ( myRandomText .length < 500){
+    myRandomText+= "abc"
+
+}
+console.log (myRandomText);
+
+ myRandomText =  "abc";
+
+while ( myRandomText .length < 50000){
+    myRandomText+= "abc"
+
+}
+console.log (myRandomText);
