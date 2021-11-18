7d7af7d8fff8de88e477394e6eef7d41948528fd
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-18 13:27:47 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,16 @@
+let myRandomText = "abc";
+while (myRandomText.length <= 500) {
+    myRandomText = myRandomText + "a"+"b"+"c";
+    
+    
+}
+console.log(myRandomText);
+
+
+let myRandomText2 = "abc";
+while (myRandomText2.length <= 50000) {
+    myRandomText2 = myRandomText2 + "a"+"b"+"c";
+    
+    
+}
+console.log(myRandomText2);
